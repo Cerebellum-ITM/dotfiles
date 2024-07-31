987f6313bce11a55b70ad64f1724599b5a2126df
Pascual Chávez <pascual.chavez@tecmor.mx>
2024-07-31 12:57:06 -0600

    [FIX] fzf-make.sh: Fixed an issue preventing some commands from not working due to 'xargs'

--- scripts/fzf-make.sh
+++ scripts/fzf-make.sh
@@ -5,16 +5,26 @@ check_makefile() {
     fi
 }
 _fzf_make_gui() {
-    fzf-tmux -m -p80%,60% -- \
+    fzf-tmux --ansi	-m -p80%,60% -- \
         --layout=reverse --multi --height=50% --min-height=20 --border \
         --border-label-pos=2 \
         --color='header:italic:underline,label:blue' \
-        --preview-window='right,50%,border-left' \
-        --header='Select a target to run' \
-        --preview="awk '/^{}[[:space:]]*:/ {flag=1; next} /^[^[:space:]]+:/ {flag=0} flag && /^[[:space:]]/' Makefile | bat --style=plain --language=shell --paging=always --pager=less"
+        --preview-window='right,80%,border-left' \
+        --header='Select the commands to run' \
+        --preview="awk '/^{}[[:space:]]*:/ {flag=1; next} /^[^[:space:]]+:/ {flag=0} flag && /^[[:space:]]/' Makefile | sed 's/^\t//' | bat --style='${BAT_STYLE:-full}' --color=always --paging=always --pager='less -FRX' --language=sh"
 }
 
 _funtion_list() {
     check_makefile || return 1
-    grep -E '^[^[:space:]]+:' Makefile | grep -v '^.PHONY' | cut -d: -f1 | _fzf_make_gui | xargs -I{} make {}
+    local selected_commands=$(grep -E '^[^[:space:]]+:' Makefile | grep -v '^.PHONY' | cut -d: -f1 | _fzf_make_gui)
+    if [ -n "$selected_commands" ]; then
+        IFS=$'\n' commands_array=()
+        while IFS= read -r line; do
+            commands_array+=("$line")
+        done <<< "$selected_commands"
+        
+        for cmd in "${commands_array[@]}"; do
+            make "$cmd"
+        done
+    fi
 }
