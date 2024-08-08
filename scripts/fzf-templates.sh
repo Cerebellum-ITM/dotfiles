8f3323a08794e72ff48aa693beab04f129525e4c
Pascual Chávez <pascual.chavez@tecmor.mx>
2024-08-08 14:44:14 -0600

    [ADD] fzf-templates: Added flow to generate a Makefile in the current directory

--- scripts/fzf-templates.sh
+++ scripts/fzf-templates.sh
@@ -10,5 +10,22 @@ _fzf_templates_gui() {
 _odoo_template_list() {
     BASE_DIR="$HOME/dotfiles/templates/odoo"
     SELECTED_DIR=$(find "$BASE_DIR" -mindepth 1 -maxdepth 1 -type d -exec basename {} \; | _fzf_templates_gui)
-    cookiecutter $HOME/dotfiles/templates/odoo/$SELECTED_DIR
+    if [ "$SELECTED_DIR" = "makefile_template" ]; then
+        ODOO_CONTAINER_NAME=$(docker-compose ps | fzf --header-lines=2 --header='Select the name of: Odoo Container' --layout=reverse --color='header:italic:underline:red,label:green' --preview-window=hidden| awk '{print $1}')
+        DB_CONTAINER_NAME=$(docker-compose ps | fzf --header-lines=2 --header='Select the name of: Db Container' --layout=reverse --color='header:italic:underline:red,label:green' --preview-window=hidden| awk '{print $1}')
+        TEMPLATE_FILE="$BASE_DIR/$SELECTED_DIR/Makefile"
+        OUTPUT_FILE="Makefile"
+        cp "$TEMPLATE_FILE" "$OUTPUT_FILE"
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+            sed -i '' -e "s/{{ cookiecutter.odoo_container }}/$ODOO_CONTAINER_NAME/g" \
+                -e "s/{{ cookiecutter.db_container }}/$DB_CONTAINER_NAME/g" \
+            "$OUTPUT_FILE"
+        else
+            sed -i -e "s/{{ cookiecutter.odoo_container }}/$ODOO_CONTAINER_NAME/g" \
+                -e "s/{{ cookiecutter.db_container }}/$DB_CONTAINER_NAME/g" \
+            "$OUTPUT_FILE"
+        fi
+    else
+        cookiecutter $HOME/dotfiles/templates/odoo/$SELECTED_DIR
+    fi
 }
