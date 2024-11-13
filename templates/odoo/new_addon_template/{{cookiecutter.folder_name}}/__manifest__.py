094cb3fd3ce34f1ea7065cec0cf5e5e40b8a4b60
Pascual Chávez <pascual.chavez@tecmor.mx>
2024-11-13 13:09:27 -0600

    [ADD] new_addon_template: a new template was added to create a base for Odoo modules

---
+++ templates/odoo/new_addon_template/{{cookiecutter.folder_name}}/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': '{{ cookiecutter.module_name }}',
+    'summary': '''
+        {{ cookiecutter.module_description }}
+    ''',
+    'category': '{{ cookiecutter.category }}',
+    'version': '{{ cookiecutter.odoo_version }}.0.0.1',
+    'depends': [
+        '{{ cookiecutter.depends }}',
+    ],
+    'data': [
+        # './security/groups.xml',
+        # './security/ir.model.access.csv',
+    ],
+    'assets': {
+
+    },
+    'external_dependencies': {
+        'python':[{{ cookiecutter.external_dependencies }}],
+    },
+    'license': '{{ cookiecutter.license }}',
+    'installable': {{ cookiecutter.installable }},
+    'application': {{ cookiecutter.application }},
+    'auto_install': {{ cookiecutter.auto_install }},
+    'author': "{{ cookiecutter.author_name }}",
+}
+
