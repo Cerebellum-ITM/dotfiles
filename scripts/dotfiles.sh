dae699060e30c0c4059a87ebbc6ca7be6c52f175
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-01-14 18:33:15 -0600

    [FIX] dotfiles.sh: added protection to the entire dotfiles function

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -9,11 +9,11 @@ function dotfiles_update() {
 
 function dotfiles() {
     if [[ "$1" == "update" || "$1" == "-u" ]]; then
-        cd $HOME/dotfiles
-        git pull
-        source ~/.zshrc
-        cd -
+        cd $HOME/dotfiles || { echo "Failed to cd to $HOME/dotfiles"; return 1; }
+        git pull || { echo "Failed to pull from git"; return 1; }
+        source ~/.zshrc || { echo "Failed to source ~/.zshrc"; return 1; }
+        cd - || { echo "Failed to return to previous directory"; return 1; }
     elif [[ "$1" == "install" || "$1" == "-ins" ]]; then
-        ansible-playbook  $HOME/dotfiles/ansible/sites.yml -i $HOME/dotfiles/ansible/inventory.ini
+        ansible-playbook $HOME/dotfiles/ansible/sites.yml -i $HOME/dotfiles/ansible/inventory.ini || { echo "Ansible playbook failed"; return 1; }
     fi
 }
