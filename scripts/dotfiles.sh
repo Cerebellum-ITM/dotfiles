791816bf66f2b9aca6d2c6f892a86d9dfe3e6cbd
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-01-15 12:14:51 -0600

    [IMP] dotfiles.sh: better use the dotfile update command

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -4,7 +4,7 @@ function dotfiles_update() {
     ./install.sh --unattended
     history_clean
     source ~/.zshrc
-    cd -
+    cd - > /dev/null 2>&1
 }
 
 function dotfiles() {
@@ -13,7 +13,7 @@ function dotfiles() {
         cd $HOME/dotfiles || { echo "Failed to cd to $HOME/dotfiles"; return 1; }
         git pull || { echo "Failed to pull from git"; return 1; }
         source ~/.zshrc || { echo "Failed to source ~/.zshrc"; return 1; }
-        cd - || { echo "Failed to return to previous directory"; return 1; }
+        cd - > /dev/null 2>&1 || { echo "Failed to return to previous directory"; return 1; }
     elif [[ "$1" == "install" || "$1" == "-ins" ]]; then
         ansible-playbook $HOME/dotfiles/ansible/sites.yml -i $HOME/dotfiles/ansible/inventory.ini || { echo "Ansible playbook failed"; return 1; }
     fi
