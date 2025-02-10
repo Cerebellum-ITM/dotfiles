45ef40505559548aa996d595b4e323759afbcc07
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-02-10 09:03:21 -0600

    [ADD] nvim: Script installation methods were added in the tools folder

--- tools/install_legacy.sh
+++ tools/install_legacy.sh
@@ -5,7 +5,7 @@ if [ "$os_name" = "Linux" ]; then
     distribution=$(grep '^NAME=' /etc/os-release | cut -d= -f2 | tr -d '"')
 fi
 
-if ! command -v gum &> /dev/null; then
+if ! command -v gum &>/dev/null; then
     echo "Installing gum"
     if [ "$os_name" = "Linux" ]; then
         sudo mkdir -p /etc/apt/keyrings
@@ -20,7 +20,6 @@ else
 fi
 # shellcheck source=/dev/null
 source "$HOME"/dotfiles/tools/gum_log_functions.sh
-
 
 # Ask Y/n
 function ask() {
@@ -38,21 +37,21 @@ function ask() {
 gum_log_info "Check for dependencies"
 # install all dependencies
 
-if [ "$os_name" = "Linux" ]; then   
+if [ "$os_name" = "Linux" ]; then
     if [ "$distribution" = "Ubuntu" ] || [ "$distribution" = "Debian GNU/Linux" ]; then
         sudo apt-get update
     elif [ "$distribution" = "Amazon Linux" ]; then
         sudo yum update
     fi
 elif [ "$os_name" = "Darwin" ]; then
-    if ! command -v brew &> /dev/null; then
+    if ! command -v brew &>/dev/null; then
         gum_log_debug "Installing Homebrew"
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
     brew update
 fi
 
-if ! command -v zsh &> /dev/null; then
+if ! command -v zsh &>/dev/null; then
     gum_log_debug "Installing zsh"
     if [ "$os_name" = "Linux" ]; then
         if [ "$distribution" = "Ubuntu" ] || [ "$distribution" = "Debian GNU/Linux" ]; then
@@ -67,7 +66,7 @@ else
     gum_log_info "zsh is already installed"
 fi
 
-if ! command -v stow &> /dev/null; then
+if ! command -v stow &>/dev/null; then
     gum_log_debug "Installing stow"
     if [ "$os_name" = "Linux" ]; then
         if [ "$distribution" = "Ubuntu" ] || [ "$distribution" = "Debian GNU/Linux" ]; then
@@ -90,7 +89,7 @@ else
     gum_log_info "Stow is already installed"
 fi
 
-if ! command -v oh-my-posh &> /dev/null; then
+if ! command -v oh-my-posh &>/dev/null; then
     gum_log_debug "Installing oh-my-posh"
     if [ "$os_name" = "Linux" ]; then
         sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
@@ -107,7 +106,7 @@ else
     # sudo oh-my-posh upgrade --force
 fi
 
-if ! command -v fzf &> /dev/null; then
+if ! command -v fzf &>/dev/null; then
     gum_log_debug "Installing fzf"
     if [ "$os_name" = "Linux" ]; then
         git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -119,7 +118,7 @@ else
     gum_log_info "fzf is already installed"
 fi
 
-if ! command -v zoxide &> /dev/null; then
+if ! command -v zoxide &>/dev/null; then
     gum_log_debug "Installing zoxide"
     if [ "$os_name" = "Linux" ]; then
         curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
@@ -130,12 +129,12 @@ else
     gum_log_info "zoxide is already installed"
 fi
 
-if ! command -v bat &> /dev/null; then
+if ! command -v bat &>/dev/null; then
     gum_log_debug "Installing bat"
     if [ "$os_name" = "Linux" ]; then
         if [ "$distribution" = "Ubuntu" ] || [ "$distribution" = "Debian GNU/Linux" ]; then
             sudo apt-get install bat -y
-            if ! command -v bat &> /dev/null; then
+            if ! command -v bat &>/dev/null; then
                 mkdir -p ~/.local/bin
                 ln -s /usr/bin/batcat ~/.local/bin/bat
             fi
@@ -144,7 +143,7 @@ if ! command -v bat &> /dev/null; then
             curl -o bat.zip -L https://github.com/sharkdp/bat/releases/download/v0.24.0/bat-v0.24.0-x86_64-unknown-linux-musl.tar.gz
             tar -xvf bat.zip
             mv bat-v0.24.0-x86_64-unknown-linux-musl /usr/bin/batcat
-            ln -s /usr/bin/batcat/bat ~/.local/bin/bat 
+            ln -s /usr/bin/batcat/bat ~/.local/bin/bat
         fi
     elif [ "$os_name" = "Darwin" ]; then
         brew install bat
@@ -153,7 +152,7 @@ else
     BAT_MIN_VERSION="0.24.0"
     BAT_CURRENT_VERSION=$(bat --version | awk '{print $2}')
     if [ "$(printf '%s\n' "$BAT_MIN_VERSION" "$BAT_CURRENT_VERSION" | sort -V | head -n1)" != "$BAT_MIN_VERSION" ]; then
-    gum_log_warning "The installed version of bat is lower than the minimum required version" CURRENT_VERSION "$BAT_CURRENT_VERSION" BAT_MIN_VERSION $BAT_MIN_VERSION
+        gum_log_warning "The installed version of bat is lower than the minimum required version" CURRENT_VERSION "$BAT_CURRENT_VERSION" BAT_MIN_VERSION $BAT_MIN_VERSION
         if [ "$os_name" = "Linux" ]; then
             if [ "$distribution" = "Ubuntu" ]; then
                 gum_gum_log_debug "Installing for distribution:" "$distribution"
@@ -165,13 +164,13 @@ else
                 curl -o bat.zip -L https://github.com/sharkdp/bat/releases/download/v0.25.0/bat-v0.25.0-aarch64-unknown-linux-gnu.tar.gz
                 tar -xvf bat.zip
                 mv bat-v0.24.0-x86_64-unknown-linux-musl /usr/bin/batcat
-                ln -s /usr/bin/batcat/bat ~/.local/bin/bat 
+                ln -s /usr/bin/batcat/bat ~/.local/bin/bat
             elif [ "$distribution" = "Amazon Linux" ]; then
                 gum_gum_log_debug "Installing for distribution:" "$distribution"
                 curl -o bat.zip -L https://github.com/sharkdp/bat/releases/download/v0.25.0/bat-v0.25.0-x86_64-unknown-linux-musl.tar.gz
                 tar -xvf bat.zip
                 mv bat-v0.24.0-x86_64-unknown-linux-musl /usr/bin/batcat
-                ln -s /usr/bin/batcat/bat ~/.local/bin/bat 
+                ln -s /usr/bin/batcat/bat ~/.local/bin/bat
             fi
         elif [ "$os_name" = "Darwin" ]; then
             brew install bat
@@ -185,7 +184,7 @@ else
     bat cache --build
 fi
 
-if ! command -v eza &> /dev/null; then
+if ! command -v eza &>/dev/null; then
     gum_log_debug "Installing eza"
     if [ "$os_name" = "Linux" ]; then
         sudo apt update
@@ -205,9 +204,9 @@ else
     sudo eza upgrade --force
 fi
 
-if ! command -v cookiecutter &> /dev/null; then
-    if command -v python3 &> /dev/null; then
-        if ! command -v pip &> /dev/null; then
+if ! command -v cookiecutter &>/dev/null; then
+    if command -v python3 &>/dev/null; then
+        if ! command -v pip &>/dev/null; then
             gum_log_debug "pip is not installed. Installing pip."
             sudo apt-get install -y python3-pip
         fi
@@ -242,7 +241,7 @@ fi
 GIT_MIN_VERSION="2.5.0"
 GIT_CURRENT_VERSION=$(git --version | awk '{print $3}')
 if [ "$(printf '%s\n' "$GIT_MIN_VERSION" "$GIT_CURRENT_VERSION" | sort -V | head -n1)" != "$GIT_MIN_VERSION" ]; then
-gum_log_warning "The installed version of git is lower than the minimum required version" CURRENT_VERSION "$GIT_CURRENT_VERSION" BAT_MIN_VERSION "$GIT_MIN_VERSION"
+    gum_log_warning "The installed version of git is lower than the minimum required version" CURRENT_VERSION "$GIT_CURRENT_VERSION" BAT_MIN_VERSION "$GIT_MIN_VERSION"
     if [ "$os_name" = "Linux" ]; then
         gum_log_debug "Installing for distribution:" "$distribution"
         sudo add-apt-repository ppa:git-core/ppa
@@ -257,7 +256,7 @@ else
 fi
 
 #! Check for translation dependencies
-if ! command -v trans &> /dev/null; then
+if ! command -v trans &>/dev/null; then
     gum_log_debug "Installing trans"
     if [ "$os_name" = "Linux" ]; then
         if [ "$distribution" = "Ubuntu" ]; then
@@ -273,7 +272,7 @@ else
 fi
 
 #! Install delta
-if ! command -v delta &> /dev/null; then
+if ! command -v delta &>/dev/null; then
     gum_log_debug "Installing delta"
     if [ "$os_name" = "Linux" ]; then
         wget https://github.com/dandavison/delta/releases/download/0.18.2/git-delta_0.18.2_amd64.deb
@@ -282,14 +281,14 @@ if ! command -v delta &> /dev/null; then
     elif [ "$os_name" = "Darwin" ]; then
         brew install git-delta
     fi
-    cat "$HOME"/dotfiles/git/delta_config.txt >> "$HOME/.gitconfig"
+    cat "$HOME"/dotfiles/git/delta_config.txt >>"$HOME/.gitconfig"
 else
     DELTA_MIN_VERSION="0.24.0"
     GITCONFIG="$HOME/.gitconfig"
     DELTA_CONFIG="$HOME/dotfiles/git/delta_config.txt"
     DELTA_CURRENT_VERSION=$(bat --version | awk '{print $2}')
     if [ "$(printf '%s\n' "$DELTA_MIN_VERSION" "$DELTA_CURRENT_VERSION" | sort -V | head -n1)" != "$DELTA_MIN_VERSION" ]; then
-    gum_log_warning "The installed version of Delta is lower than the minimum required version" DELTA_CURRENT_VERSION "$DELTA_CURRENT_VERSION" DELTA_MIN_VERSION $DELTA_MIN_VERSION
+        gum_log_warning "The installed version of Delta is lower than the minimum required version" DELTA_CURRENT_VERSION "$DELTA_CURRENT_VERSION" DELTA_MIN_VERSION $DELTA_MIN_VERSION
         if [ "$os_name" = "Linux" ]; then
             wget https://github.com/dandavison/delta/releases/download/0.18.2/git-delta_0.18.2_amd64.deb
             sudo dpkg -i git-delta_0.18.2_amd64.deb
@@ -305,14 +304,14 @@ else
     if ! grep -q "^\[delta\]" "$GITCONFIG"; then
         gum_log_warning "Delta configuration is missing"
         gum_log_debug "Adding Delta configuration"
-        cat "$HOME"/dotfiles/git/delta_config.txt >> "$HOME/.gitconfig"
+        cat "$HOME"/dotfiles/git/delta_config.txt >>"$HOME/.gitconfig"
     fi
     gum_log_info "Delta is already installed"
 fi
 
-if ! command -v hx &> /dev/null; then
+if ! command -v hx &>/dev/null; then
     gum_log_debug "Installing Helix"
-    if [ "$os_name" = "Linux" ]; then        
+    if [ "$os_name" = "Linux" ]; then
         curl -LO https://github.com/helix-editor/helix/releases/download/25.01.1/helix-25.01.1-x86_64-linux.tar.xz
         tar -xf helix-25.01.1-x86_64-linux.tar.xz
         cd helix-25.01.1-x86_64-linux && sudo mv -f hx /usr/local/bin/ && sudo mv -f runtime/ /usr/local/bin/
@@ -324,9 +323,9 @@ else
     gum_log_info "Helix is already installed"
 fi
 
-if ! command -v lazygit &> /dev/null; then
+if ! command -v lazygit &>/dev/null; then
     gum_log_debug "Installing lazygit"
-    if [ "$os_name" = "Linux" ]; then        
+    if [ "$os_name" = "Linux" ]; then
         LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
         curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
         tar xf lazygit.tar.gz lazygit
@@ -338,13 +337,25 @@ else
     gum_log_info "Helix is already installed"
 fi
 
-if ! command -v atuin &> /dev/null; then
+if ! command -v atuin &>/dev/null; then
     gum_log_debug "Installing Atuin"
     curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
 else
     gum_log_info "Atuin is already installed"
 fi
 
+if ! command -v nvim &>/dev/null; then
+    gum_log_debug "Installing nvim"
+    if [ "$os_name" = "Linux" ]; then
+        curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+        sudo rm -rf /opt/nvim
+        sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+    elif [ "$os_name" = "Darwin" ]; then
+        brew install neovim
+    fi
+else
+    gum_log_info "Helix is already installed"
+fi
 
 #* Check if history files exist, if not, create them
 FZF_MAKE_HISTORY_FILE="$HOME/dotfiles/home/.config/.tmp/.fzf-make_history.log"
@@ -364,11 +375,9 @@ fi
 DOCKER_COMPOSE_CONFIG_FILE="$HOME/dotfiles/home/.config/.tmp/.docker-compose-config"
 if [ ! -f "$DOCKER_COMPOSE_CONFIG_FILE" ]; then
     touch "$DOCKER_COMPOSE_CONFIG_FILE"
-    echo "export DOCKER_COMPOSE_CMD='docker compose'" > "$DOCKER_COMPOSE_CONFIG_FILE"
+    echo "export DOCKER_COMPOSE_CMD='docker compose'" >"$DOCKER_COMPOSE_CONFIG_FILE"
     gum_log_info "Created docker-compose config file"
 fi
-
-
 
 UNATTENDED_INSTALLATION=false
 if [ "$1" == "--unattended" ]; then
