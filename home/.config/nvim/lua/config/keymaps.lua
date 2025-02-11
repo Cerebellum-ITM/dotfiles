9e751c62fd9ef62cf417c2d321eafcaa42de4cb3
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-02-10 22:57:28 -0600

    [ADD] nvim: Key shortcuts were added to move whole lines

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,5 @@
 vim.keymap.set("n", "<Tab>", ">>", { desc = "Indent line" })
 vim.keymap.set("n", "<S-Tab>", "<<", { desc = "Unindent line" })
 vim.keymap.set("n", "<C-c>", "<Plug>(comment_toggle_linewise_current)", { desc = "Comment current line" })
+vim.keymap.set("n", "<S-Up>", ":m .-1<CR>==", { desc = "Move line up" })
+vim.keymap.set("n", "<S-Down>", ":m .+1<CR>==", { desc = "Move line down" })
