205e79031746d77b3b8400d1de9fe52624b64a39
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-02-13 07:24:39 -0600

    [ADD] nvim: An option was added so that Yanks actions are passed to the clipboard

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -5,3 +5,4 @@ vim.o.tabstop = 4 -- A Tab character will be 4 spaces wide.
 vim.o.shiftwidth = 4 -- Indentation levels will be 4 spaces.
 vim.o.softtabstop = 4 -- Tab key inserts 4 spaces.
 vim.o.expandtab = true -- Convert tabs to spaces.
+vim.o.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim. (default: '')
