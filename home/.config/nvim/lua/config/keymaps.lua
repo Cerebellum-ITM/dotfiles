42e016a96d97d4aa6f958585cffe975a0578e6b1
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-02-13 09:44:59 -0600

    [FIX] nvim: The commands used to exchange lines were fixed

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -4,8 +4,8 @@
 vim.keymap.set("n", "<Tab>", ">>", { desc = "Indent line" })
 vim.keymap.set("n", "<S-Tab>", "<<", { desc = "Unindent line" })
 vim.keymap.set("n", "<C-c>", "<Plug>(comment_toggle_linewise_current)", { desc = "Comment current line" })
-vim.keymap.set("n", "<S-Up>", ":m .-1<CR>==", { desc = "Move line up" })
-vim.keymap.set("n", "<S-Down>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<S-Up>", "<cmd>m .-2<CR>==", { desc = "Move line up" })
+vim.keymap.set("n", "<S-Down>", "<cmd>m .+1<CR>==", { desc = "Move line down" })
 
 -- For conciseness
 local opts = { noremap = true, silent = true }
