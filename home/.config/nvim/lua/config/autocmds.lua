403e2c85a2fd3aa7bafcf7210a32fb73e554e972
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-02-19 10:53:22 -0600

    [ADD] nvim: A configuration was added so that Lua files have different treating when writing

--- home/.config/nvim/lua/config/autocmds.lua
+++ home/.config/nvim/lua/config/autocmds.lua
@@ -11,3 +11,16 @@ vim.api.nvim_create_autocmd("BufWinLeave", {
   command = "silent! :wa",
   desc = "Auto-save on Focus Lost",
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "lua",
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+    vim.opt_local.textwidth = 160
+    vim.opt_local.fileformat = "unix"
+  end,
+  desc = "Lua Filetype Options"
+})
