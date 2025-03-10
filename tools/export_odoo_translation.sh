27b0be5444a3c065b92192cb3e90d247e6e7419b
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-03-10 12:01:40 -0600

    [FIX] fzf-make.sh: Translation export flow is completed

--- tools/export_odoo_translation.sh
+++ tools/export_odoo_translation.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+# shellcheck disable=SC1091
+
+source "$HOME/dotfiles/tools/gum_styles.sh"
+source "$HOME/dotfiles/tools/gum_log_functions.sh"
 
 FILE_NAME="es_MX.po"
-MODULE_PATH_FOLDER=$1
-DOCKER_CONTAINER_PATH=$2
-if [[ ! -d "$MODULE_PATH_FOLDER/i18n" ]]; then
-    mkdir -p "$MODULE_PATH_FOLDER/i18n"
+MODULE_FOLDER=$1
+REPOSITORY_NAME=$2
+WORKING_DIR=$3
+
+gum_log_info "$(gum_blue " ") $(git_strong_gray_light "$MODULE_FOLDER")"
+gum_log_info "$(gum_blue " ") $(git_strong_gray_light "$REPOSITORY_NAME")"
+gum_log_info "$(gum_blue " ") $(git_strong_gray_light "$WORKING_DIR")"
+if [[ ! -d "$WORKING_DIR/$REPOSITORY_NAME/$MODULE_FOLDER/i18n" ]]; then
+    gum_log_debug "$(gum_blue " ") $(git_strong_gray_light "Creating translations directory")"
+    mkdir -p "$WORKING_DIR/$REPOSITORY_NAME/$MODULE_FOLDER/i18n"
 fi
 
-if mv -f "$DOCKER_CONTAINER_PATH/$FILE_NAME" "$MODULE_PATH_FOLDER/i18n/$FILE_NAME"; then
-    echo "The translation was copied correctly to $MODULE_PATH_FOLDER"
+if mv -f "$WORKING_DIR/$FILE_NAME" "$WORKING_DIR/$REPOSITORY_NAME/$MODULE_FOLDER/i18n/$FILE_NAME"; then
+    gum_log_info "$(gum_blue " ") $(git_strong_gray_light "The translation was copied correctly to $MODULE_FOLDER")"
 else
-    echo "There was an error while copying the translation"
+    gum_log_error "$(gum_blue " ") $(git_strong_gray_light "There was an error while copying the translation")"
 fi
