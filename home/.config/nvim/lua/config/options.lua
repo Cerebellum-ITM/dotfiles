7faf0b5d4728d0512e81ae54fa3f92bab073234e
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-04-04 10:45:45 -0600

    [ADD] nvim: Now the cursor blink

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -7,3 +7,4 @@ vim.o.softtabstop = 4 -- Tab key inserts 4 spaces.
 vim.o.expandtab = true -- Convert tabs to spaces.
 vim.o.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim. (default: '')
 vim.opt.spelllang = "en_us,es_mx"
+vim.opt.guicursor = "a:blinkon100"
