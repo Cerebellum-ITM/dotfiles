1b41fbb307253f2a53b01b8d57b1d92105fb1416
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-04-11 14:55:04 -0600

    [FIX] nvim: The way in which the mini.split commands are called was corrected

--- home/.config/nvim/lua/plugins/mini-splitjoin.lua
+++ home/.config/nvim/lua/plugins/mini-splitjoin.lua
@@ -1,9 +1,14 @@
 local wk = require("which-key")
 wk.add({
   { "gs", group = "splitjoin", icon = "" },
-  { "gsj", "<cmd>MiniSplitjoinToggle<CR>", desc = "Toggle Split/Join", icon = { icon = "", color = "orange" } },
-  { "gss", "<cmd>MiniSplitjoinSplit<CR>", desc = "Split", icon = "󰉸" },
-  { "gsJ", "<cmd>MiniSplitjoinJoin<CR>", desc = "Join", icon = "󰘦" },
+  {
+    "gsj",
+    "<cmd>lua MiniSplitjoin.toggle()<CR>",
+    desc = "Toggle Split/Join",
+    icon = { icon = "", color = "orange" },
+  },
+  { "gss", "<cmd>lua MiniSplitjoin.split()<CR>", desc = "Split", icon = "󰉸" },
+  { "gsJ", "<cmd>lua MiniSplitjoin.join()<CR>", desc = "Join", icon = "󰘦" },
 })
 
 return {
