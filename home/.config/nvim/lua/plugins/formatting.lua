b653c2b30dc1bf1e8b304bc9237cc390357721e9
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-04-16 12:23:09 -0600

    [ADD] nvim: Automatic formatting for XML files was added

---
+++ home/.config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,18 @@
+return {
+  "stevearc/conform.nvim",
+  opts = function(_, opts)
+    opts.formatters_by_ft = opts.formatters_by_ft or {}
+    opts.formatters_by_ft.xml = { "xmlformatter" }
+
+    opts.formatters = opts.formatters or {}
+    opts.formatters.xmlformatter = {
+      prepend_args = {
+        "--indent",
+        "4",
+        "--indent-char",
+        " ",
+        "--selfclose",
+      },
+    }
+  end,
+}
