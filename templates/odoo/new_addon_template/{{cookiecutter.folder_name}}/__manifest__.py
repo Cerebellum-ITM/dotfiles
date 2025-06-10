8670cc8505b5fdc1d42e70e954564968a4a861ec
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-06-10 12:57:33 -0600

    [IMP] new_addon_template: The template for the manifest file was improved

--- templates/odoo/new_addon_template/{{cookiecutter.folder_name}}/__manifest__.py
+++ templates/odoo/new_addon_template/{{cookiecutter.folder_name}}/__manifest__.py
@@ -1,28 +1,33 @@
 # -*- coding: utf-8 -*-
+
+# _____  _
+# |  __ \(_)
+# | |  | |___   _____ _ __ ______ _
+# | |  | | \ \ / / _ \ '__|_  / _` |
+# | |__| | |\ V /  __/ |   / / (_| |
+# |_____/|_| \_/ \___|_|  /___\__,_|
+
 {
-    'name': '{{ cookiecutter.module_name }}',
-    'summary': '''
+    "name": "{{ cookiecutter.module_name }}",
+    "summary": """
         {{ cookiecutter.module_description }}
-    ''',
-    'category': '{{ cookiecutter.category }}',
-    'version': '{{ cookiecutter.odoo_version }}.0.0.1',
-    'depends': [
-        '{{ cookiecutter.depends }}',
+    """,
+    "category": "{{ cookiecutter.category }}",
+    "version": "{{ cookiecutter.odoo_version }}.0.0.1",
+    "depends": [
+        "{{ cookiecutter.depends }}",
     ],
-    'data': [
+    "data": [
         # './security/groups.xml',
         # './security/ir.model.access.csv',
     ],
-    'assets': {
-
+    "assets": {},
+    "external_dependencies": {
+        "python": ["{{ cookiecutter.external_dependencies }}"],
     },
-    'external_dependencies': {
-        'python':['{{ cookiecutter.external_dependencies }}'],
-    },
-    'license': '{{ cookiecutter.license }}',
-    'installable': {{ cookiecutter.installable }},
-    'application': {{ cookiecutter.application }},
-    'auto_install': {{ cookiecutter.auto_install }},
-    'author': "{{ cookiecutter.author_name }}",
-}
-
+    "license": "{{ cookiecutter.license }}",
+    "installable": {{cookiecutter.installable}},
+    "application": {{cookiecutter.application}},
+    "auto_install": {{cookiecutter.auto_install}},
+    "author": "{{ cookiecutter.author_name }}",
+}  # pyright: ignore[reportUnusedExpression]
