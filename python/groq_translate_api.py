d8b809899c663005d7997835270bdd639a4d66f3
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-06-24 18:14:43 -0600

    [ADD] python: A new script was added to translate through AI

---
+++ python/groq_translate_api.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import argparse
+
+from groq import Groq
+from rich import print
+from dotenv import load_dotenv
+
+
+def translate_commit_message(commit_message):
+    load_dotenv(".env")
+
+    client = Groq(
+        api_key=os.environ.get("GROQ_API_KEY"),
+    )
+
+    system_prompt = """
+        Translate the following Git commit message from Spanish to English. The message was written by a developer with extensive experience. Ensure the translation maintains technical context and precision, while keeping the translated message between 80 and 150 characters.
+    """
+
+    try:
+        completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "system",
+                    "content": system_prompt,
+                },
+                {"role": "user", "content": commit_message},
+            ],
+            model="llama-3.3-70b-versatile",
+        )
+
+        return completion.choices[0].message.content
+
+    except Exception as e:
+        print(f"[red]Error translating commit message: {e}[/red]")
+        sys.exit(1)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Translate Git commit messages from Spanish to English."
+    )
+    parser.add_argument("message", type=str, help="The commit message to translate")
+
+    args = parser.parse_args()
+    translated_message = translate_commit_message(args.message)
+    print(translated_message)
+
+
+if __name__ == "__main__":
+    main()
+
