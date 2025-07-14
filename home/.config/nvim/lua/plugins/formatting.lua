052638dc6fc0e928b0fbbb7ce590b1fa11e2060f
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-07-14 23:13:24 +0000

    [IMP] nvim: Changed XML file formatter

--- home/.config/nvim/lua/plugins/formatting.lua
+++ home/.config/nvim/lua/plugins/formatting.lua
@@ -3,33 +3,39 @@ return {
   opts = function(_, opts)
     opts.formatters_by_ft = opts.formatters_by_ft or {}
     opts.formatters = opts.formatters or {}
-
-    opts.formatters_by_ft.xml = { "xmlformatter" }
-    opts.formatters.xmlformatter = {
-      prepend_args = {
-        "--indent",
-        "4",
-        "--indent-char",
-        " ",
-        "--selfclose",
-        "--eof-newline",
-        "--preserve-attributes",
-        "--blanks",
-      },
-    }
-
     opts.formatters_by_ft.javascript = { "prettier" }
-    opts.formatters.prettier = {
-      prepend_args = {
-        "--tab-width",
-        "4",
-        "--single-quote",
-        "--jsx-single-quote",
-        "--trailing-comma",
-        "all",
-        "--use-tabs",
-        "false",
-      },
+    opts.formatters_by_ft.xml = { "prettier" }
+    opts.formatters["prettier"] = {
+      prepend_args = function(self, ctx)
+        local args
+        if ctx.filename:match("%.xml$") then
+          args = {
+            "--print-width",
+            "180",
+            "--bracket-same-line",
+            "true",
+            "--tab-width",
+            "4",
+            "--single-attribute-per-line",
+            "false",
+            "--xml-sort-attributes-by-key",
+            "false",
+            "--plugin=@prettier/plugin-xml",
+          }
+        else
+          args = {
+            "--tab-width",
+            "4",
+            "--single-quote",
+            "--jsx-single-quote",
+            "--trailing-comma",
+            "all",
+            "--use-tabs",
+            "false",
+          }
+        end
+        return args
+      end,
     }
 
     opts.formatters_by_ft.go = { "gofumpt", "goimports_reviser", "golines" }
