2f08430be2a77f1d223b64adac3e501821dcce28
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-07-31 10:28:30 -0600

    [FIX] lazygit-custom-upstream-workflow.sh: Added description log on script initialization

--- tools/lazygit-custom-upstream-workflow.sh
+++ tools/lazygit-custom-upstream-workflow.sh
@@ -9,6 +9,6 @@ source "$HOME/dotfiles/tools/fzf-git-sourced.sh"
 source "$HOME/dotfiles/scripts/fzf-git-custom.sh"
 
 read -r branch
-echo " $(gum_yellow_bold_underline "${branch}")"
+echo " The upstream process for the $(gum_yellow_bold_underline "${branch}" branch has begun)"
 remote=$(_check_remote_source)
 _push_to_repository "remote=$remote" "branch=$branch" "flag=-u"
