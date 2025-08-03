d37297927e0960563a52e8b7eed6c5b16f7ba7bf
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-08-03 12:33:06 -0600

    [IMP] lazygit-custom-upstream-workflow.sh: Improved script for use as upstream and push to specific remote

--- tools/lazygit-custom-upstream-workflow.sh
+++ tools/lazygit-custom-upstream-workflow.sh
@@ -8,7 +8,28 @@ source "$HOME/dotfiles/scripts/custom_functions.sh"
 source "$HOME/dotfiles/tools/fzf-git-sourced.sh"
 source "$HOME/dotfiles/scripts/fzf-git-custom.sh"
 
+has_upstream() {
+    git rev-parse --abbrev-ref --symbolic-full-name "${branch}@{u}" >/dev/null 2>&1
+}
+
 read -r branch
-echo " The upstream process for the $(gum_yellow_bold_underline "${branch}" branch has begun)"
 remote=$(_check_remote_source)
-_push_to_repository "remote=$remote" "branch=$branch" "flag=-u"
+
+if has_upstream; then
+    flag=""
+    echo " Pushing to a remote for $(gum_green_dark_underline "${remote}")/$(gum_yellow_bold_underline "${branch}") branch has begun"
+else
+    flag="-u"
+    echo " The upstream process for the $(gum_green_dark_underline "${remote}")/$(gum_yellow_bold_underline "${branch}") branch has begun"
+fi
+
+echo $flag
+
+args=("remote=$remote" "branch=$branch")
+if [[ -n $flag ]]; then
+    args+=("flag=$flag")
+fi
+
+echo "${args[*]}"
+
+_push_to_repository "${args[@]}"
