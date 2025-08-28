5c43b9e4862a1116ec681db5e9b2ac8ce15a83f5
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-08-28 11:41:04 -0600

    [IMP] lazygit-custom-submodule-pick-commit-workflow.sh: Improve commit message parsing
    The file  was updated to modify the patterns used to extract information from
    commit messages.
    The changes address the issue where the previous code only functioned correctly
    for single-line commits.

--- tools/lazygit-custom-submodule-pick-commit-workflow.sh
+++ tools/lazygit-custom-submodule-pick-commit-workflow.sh
@@ -15,9 +15,9 @@ _check_for_git_repository_and_submodule || return 1
 echo " creating a commit in the repository $(gum_blue "$parent_folder") with the commit: $(gum_yellow_bold_underline "${commit_hash}")"
 
 commit_message=$(git log -1 --pretty=%B "$commit_hash")
-submodule_commit_type=$(echo "$commit_message" | awk -F'[] []' '{print $2}')
-file_or_folder=$(echo "$commit_message" | sed -E 's/.*\] ([^:]+):.*/\1/')
-message=$(echo "$commit_message" | sed -n 's/^\[.*\] .*: \(.*\)/\1/p')
-echo -n "[CHECKOUT-$submodule_commit_type] $file_or_folder: $message" > /tmp/fzf_git_commit
+submodule_commit_type=$(echo "$commit_message" | head -n1 | sed -E 's/^\[([^]]+)\].*/\1/')
+file_or_folder=$(echo "$commit_message" | head -n1 | sed -E 's/.*\] ([^:]+):.*/\1/')
+message=$(echo "$commit_message" | sed -E '1s/^\[[^]]*\] [^:]*: *//')
+echo -n "[CHECKOUT-$submodule_commit_type] $file_or_folder: $message" >/tmp/fzf_git_commit
 create_commit submodule
 fzf_git_check_abort || return 1
