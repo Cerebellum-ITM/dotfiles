eacff420a64a56524dfcdbc811c3fd1e31bb0b2e
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-09-17 17:10:44 -0600

    [IMP] install_commitCraft.sh: Add dynamic COMMIT_CRAFT release retrieval <BLANKLINE> The script `tools/install_commitCraft.sh` was modified to download the latest version of CommitCraft. The changes include removing the hardcoded `RELEASE_TAG` variable and instead using the GitHub API to fetch the latest release tag. The `LATEST_RELEASE_INFO` variable is used to store the API response, and the `RELEASE_TAG` is extracted from it using `grep` and `sed` commands.
    The file `tools/install_commitCraft.sh` also had some lines reordered, specifically the `case` statement and the `LATEST_RELEASE_INFO` and `RELEASE_TAG` assignments were added.

--- tools/install_commitCraft.sh
+++ tools/install_commitCraft.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 GITHUB_REPO="Cerebellum-ITM/CommitCraftReborn" # Your GitHub repository (e.g., "pascualchavez/CommitCraft_v2")
-RELEASE_TAG="v0.2.3"                           # The release tag whose source code you want to download (e.g., "v0.1.0")
-BINARY_NAME="commitcraft"                      # The name of your binary once installed
-INSTALL_DIR="$HOME/.local/bin"                 # Installation directory. Ensure it's in your PATH.
+# RELEASE_TAG="v0.2.3"                           # The release tag whose source code you want to download (e.g., "v0.1.0")
+BINARY_NAME="commitcraft"      # The name of your binary once installed
+INSTALL_DIR="$HOME/.local/bin" # Installation directory. Ensure it's in your PATH.
 
 # --- 1. Detect OS and Architecture ---
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
@@ -24,6 +24,9 @@ armv*)
     exit 1
     ;;
 esac
+
+LATEST_RELEASE_INFO=$(curl -s "https://api.github.com/repos/${GITHUB_REPO}/releases/latest")
+RELEASE_TAG=$(echo "$LATEST_RELEASE_INFO" | grep '"tag_name":' | sed -E 's/.*"tag_name": "([^"]+)".*/\1/')
 
 # --- 2. Construct the binary file name for download ---
 DOWNLOAD_FILE_NAME="commitcraft"
